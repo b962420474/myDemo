b4b59bc5934f49afc12b4c29df986058b055b151
xuexiang.yang <xuexiang.yang@iservernetwork.com>
2018-11-14 17:12:58 +0800

    代码提交

--- video/js/view/index.js
+++ video/js/view/index.js
@@ -2,15 +2,17 @@ define(['../video/video'],function(video){
 var index=new function(){
     this.init=function(){
         loadhtml(".header","./header",function(){
+            loadhtml(".content","./videoList",function(){
             
+            });
         });
-            video.init();
-            video.list=[{
-                url:'http://www.iqiyi.com/v_19rrdgiq8s.html'
-            },{
-                url:'http://v.qq.com/x/cover/og0eputlxwet1cn.html'
-            }]
-            video.play(video.list[1].url);
+            //video.init();
+            // video.list=[{
+            //     url:'http://www.iqiyi.com/v_19rrdgiq8s.html'
+            // },{
+            //     url:'http://v.qq.com/x/cover/og0eputlxwet1cn.html'
+            // }]
+            // video.play(video.list[1].url);
     }
 }
 
