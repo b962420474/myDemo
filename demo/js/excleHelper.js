0b8624f398f69eed283de04088dbc2a5d645764b
xuexiang.yang <xuexiang.yang@iservernetwork.com>
2019-01-29 16:05:20 +0800

    代码提交

---
+++ demo/js/excleHelper.js
@@ -0,0 +1,52 @@
+(function(win,$){
+    var excleHelper={
+        init:function(param){
+            var that=this;
+            $.extend(this.defaults,param);
+            $.each(this.defaults.fieldsTarget,function(i,a){
+                console.log($(a).text());
+                that.fileds.push($(a).text());
+            });
+            $.each(this.defaults.listTarget,function(i,a){
+                var data={};
+                $.each($(a).find("td"),function(j,b){
+                    console.log($(b).text());
+                    data[that.fileds[j]]=$(b).text().trim();
+                });
+                that.list.push(data);
+            });
+            that.defaults.export.target.on("click",function(){
+                that.download(that.defaults.filename+".csv");
+                that.defaults.export.clickfun&&that.defaults.export.clickfun();
+            });
+        },
+        download:function(filename){
+            var csv = json2csv.parse(this.list,this.fileds);
+            var eleLink = document.createElement('a');
+            eleLink.download = filename;
+            eleLink.style.display = 'none';
+            // 字符内容转变成blob地址
+            var blob = new Blob([csv]);
+            eleLink.href = URL.createObjectURL(blob);
+            // 触发点击
+            document.body.appendChild(eleLink);
+            eleLink.click();
+            // 然后移除
+            document.body.removeChild(eleLink);
+        },
+        defaults:{
+            filename:"",
+            fieldsTarget:null,
+            listTarget:null,
+            export:{
+                target:null,
+                clickfun:null,
+            }
+        },
+        fileds:[],
+        list:[],
+    }
+    win.excleHelper=excleHelper;
+
+})(window,jQuery)
+
--- test/ime/keybord.js
+++ test/ime/keybord.js
@@ -30,6 +30,7 @@
             "&nbsp;":" ",
             "&lt;" :"<",
             "&gt;" :">",
+            "&amp;":"&",
         },
         init:function(lang,callback){
             if(document.getElementById("keyboard")){
@@ -146,7 +147,7 @@
             });
         }
     };
-    Keyboard.layouts['rus']={
+    Keyboard.layouts['rus']={//俄语
         'name' : 'Russian',
         'lang' : 'rus',
         "lay":{
@@ -180,7 +181,7 @@
             ]
         }
     };
-    Keyboard.layouts['spa']={
+    Keyboard.layouts['spa']={//西班牙语
         'name' : 'Spanish',
         'lang' : 'spa',
         "lay":{
@@ -192,7 +193,7 @@
                 "{alt} &nbsp {alt}"
             ],
             'shift' : [
-                '\u00aa ! " \u00b7 $ % & / ( ) = ? \u00bf {bksp}',
+                '\u00aa ! " \u00b7 $ % &amp; / ( ) = ? \u00bf {bksp}',
                 "Q W E R T Y U I O P ^ *",
                 "{change} A S D F G H J K L \u00d1 \u00a8 \u00c7",
                 "{shift} Z X C V B N M ; : _ {shift}",
@@ -329,7 +330,7 @@
             "\u00b4\u00e7": "\u1e09"
         }
     };
-    Keyboard.layouts['hin']={
+    Keyboard.layouts['hin']={//印地语
         'name' : 'Hindi',
         'lang' : 'hin',
         "lay":{
@@ -363,7 +364,7 @@
             ]
         }
     };
-    Keyboard.layouts['scc']={//sr_cyrl
+    Keyboard.layouts['scc']={//sr_cyrl  塞尔维亚语
         'name' : 'Serbian',
         'lang' : 'scc',
         "lay":{
@@ -375,7 +376,7 @@
                 "{alt} &nbsp {alt}"
             ],
             'shift' : [
-                '~ ! " # $ % & / ( ) = ? * {bksp}',
+                '~ ! " # $ % &amp; / ( ) = ? * {bksp}',
                 "\u0409 \u040a \u0415 \u0420 \u0422 \u0417 \u0423 \u0418 \u041e \u041f \u0428 \u0402",
                 "{change} \u0410 \u0421 \u0414 \u0424 \u0413 \u0425 \u0408 \u041a \u041b \u0427 \u040b \u0416",
                 "{shift} \u0405 \u040f \u0426 \u0412 \u0411 \u041d \u041c ; : _ {shift}",
@@ -403,12 +404,12 @@
             "'\u041a": "\u040c"
         }
     };
-    Keyboard.layouts['fra']={//fr
+    Keyboard.layouts['fra']={//fr  法语
         'name' : 'French',
         'lang' : 'fra',
         "lay":{
             'normal' : [
-                "\u00b2 & \u00e9 \" ' ( - \u00e8 _ \u00e7 \u00e0 ) = {bksp}",
+                "\u00b2 &amp; \u00e9 \" ' ( - \u00e8 _ \u00e7 \u00e0 ) = {bksp}",
                 "a z e r t y u i o p ^ $",
                 "{change} q s d f g h j k l m \u00f9 *",
                 "{shift} w x c v b n , ; : ! {shift}",
@@ -486,7 +487,7 @@
             "^z": "\u1e91"
         }
     };
-    Keyboard.layouts['ita']={//it
+    Keyboard.layouts['ita']={//it  意大利语
         'name' : 'Italian',
         'lang' : 'ita',
         "lay":{
@@ -498,7 +499,7 @@
                 "{alt} &nbsp {alt}"
             ],
             'shift' : [
-                '| ! " \u00a3 $ % & / ( ) = ? ^ {bksp}',
+                '| ! " \u00a3 $ % &amp; / ( ) = ? ^ {bksp}',
                 "Q W E R T Y U I O P \u00e9 *",
                 "{change} A S D F G H J K L \u00e7 \u00b0 \u00a7",
                 "{shift} Z X C V B N M ; : _ {shift}",
@@ -583,7 +584,7 @@
             "^z": "\u1e91"
         }
     };
-    Keyboard.layouts['por']={//pt_pt
+    Keyboard.layouts['por']={//pt_pt   葡萄牙语
         'name' : 'Portuguese',
         'lang' : 'por',
         "lay":{
@@ -595,7 +596,7 @@
                 "{alt} &nbsp {alt}"
             ],
             'shift' : [
-                '| ! " # $ % & / ( ) = ? \u00bb {bksp}',
+                '| ! " # $ % &amp; / ( ) = ? \u00bb {bksp}',
                 "Q W E R T Y U I O P * `",
                 "{change} A S D F G H J K L \u00c7 \u00aa ^",
                 "{shift} Z X C V B N M ; : _ {shift}",
@@ -737,7 +738,7 @@
             "~y": "\u1ef9"
         }
     };
-    Keyboard.layouts['deu']={//de
+    Keyboard.layouts['deu']={//de  德语
         'name' : 'German',
         'lang' : 'deu',
         "lay":{
@@ -749,7 +750,7 @@
                 "{alt} &nbsp {alt}"
             ],
             'shift' : [
-                '\u00b0 ! \" \u00a7 $ % & / ( ) = ? ` {bksp}',
+                '\u00b0 ! \" \u00a7 $ % &amp; / ( ) = ? ` {bksp}',
                 "Q W E R T Z U I O P \u00dc *",
                 "{change} A S D F G H J K L \u00d6 \u00c4 '",
                 "{shift} Y X C V B N M ; : _ {shift}",
@@ -854,6 +855,232 @@
             "^w": "\u0175",
             "^y": "\u0177",
             "^z": "\u1e91"
+        }
+    };
+    Keyboard.layouts['urd']={//ur  乌尔都语
+        'name' : 'Urdu',
+        'lang' : 'urd',
+        "lay":{
+            'normal' : [
+                "{} \u06f1 \u06f2 \u06f3 \u06f4 \u06f5 \u06f6 \u06f7 \u06f8 \u06f9 \u06f0 - = {bksp}",
+                "\u0642 \u0648 \u0639 \u0631 \u062a \u06d2 \u0621 \u06cc \u06c1 \u067e [ ] \\",
+                "{change} \u0627 \u0633 \u062f \u0641 \u06af \u062d \u062c \u06a9 \u0644 \u061b ' {}",
+                "{shift} \u0632 \u0634 \u0686 \u0637 \u0628 \u0646 \u0645 \u060c \u06d4 / {shift}",
+                "{alt} &nbsp {alt}"
+            ],
+            'shift' : [
+                '\u064b 1 2 3 4 5 6 7 8 9 0 _ + {bksp}',
+                "\u0652 \u0651 \u0670 \u0691 \u0679 \u064e \u0626 \u0650 \u06c3 \u064f { } |",
+                '{change} \u0622 \u0635 \u0688 {} \u063a \u06be \u0636 \u062e {} : " {}',
+                "{shift} \u0630 \u0698 \u062b \u0638 {} \u06ba \u0658 {} \u066b \u061f {shift}",
+                "{alt} &nbsp {alt}"
+            ],
+            'alt' : [
+                "{} ! @ # {} \u066a {} &amp; * ( ) {} {} {bksp}",
+                "{} \u0602 \u0656 \u0613 \u0614 \u0601 \u0654 \u0611 {} \u0657 {} {} {}",
+                "{change} \ufdf2 \u0610 \ufdfa {} {} \u0612 \ufdfb {} {} {} {} {}",
+                "{shift} \u060f \u060e \u0603 {} \ufdfd \u0600 {} &lt; &gt; {} {shift}",
+                "{alt} &nbsp {alt}"
+            ],
+            'alt-shift' : [
+                "{} ! @ # {} \u066a {} &amp; * ( ) {} {} {bksp}",
+                "{} \u0602 \u0656 \u0613 \u0614 \u0601 \u0654 \u0611 {} \u0657 {} {} {}",
+                "{change} \ufdf2 \u0610 \ufdfa {} {} \u0612 \ufdfb {} {} {} {} {}",
+                "{shift} \u060f \u060e \u0603 {} \ufdfd \u0600 {} &lt; &gt; {} {shift}",
+                "{alt} &nbsp {alt}"
+            ]
+        }
+    };
+    Keyboard.layouts['dan']={//da  丹麦
+        'name' : 'Danish',
+        'lang' : 'dan',
+        "lay":{
+            'normal' : [
+                "\u00bd 1 2 3 4 5 6 7 8 9 0 + \u00b4 {bksp}",
+                "q w e r t y u i o p \u00e5 \u00a8",
+                "{change} a s d f g h j k l \u00e6 \u00f8 '",
+                "{shift} z x c v b n m , . - {shift}",
+                "{alt} &nbsp {alt}"
+            ],
+            'shift' : [
+                '\u00a7 ! " # \u00a4 % &amp; / ( ) = ? ` {bksp}',
+                "Q W E R T Y U I O P \u00c5 ^",
+                "{change} A S D F G H J K L \u00c6 \u00d8 *",
+                "{shift} Z X C V B N M ; : _ {shift}",
+                "{alt} &nbsp {alt}"
+            ],
+            'alt' : [
+                "{} &lt; @ \u00a3 $ {} {} { [ ] } {} | {bksp}",
+                "{} {} \u20ac {} {} {} {} {} {} {} {} ~",
+                "{change} {} {} {} {} {} {} {} {} {} {} {} {}",
+                "{shift} \\ {} {} {} {} {} {} {} {} {} {shift}",
+                "{alt} &nbsp {alt}"
+            ],
+            'alt-shift' : [
+                "{} &gt; @ \u00a3 $ {} {} { [ ] } {} | {bksp}",
+                "{} {} \u20ac {} {} {} {} {} {} {} {} ~",
+                "{change} {} {} {} {} {} {} {} {} {} {} {} {}",
+                "{shift} \\ {} {} {} {} {} {} {} {} {} {shift}",
+                "{alt} &nbsp {alt}"
+            ]
+        },
+        transform: {
+            "~ ": "~",
+            "~A": "\u00c3",
+            "~E": "\u1ebc",
+            "~I": "\u0128",
+            "~N": "\u00d1",
+            "~O": "\u00d5",
+            "~U": "\u0168",
+            "~V": "\u1e7c",
+            "~Y": "\u1ef8",
+            "~a": "\u00e3",
+            "~e": "\u1ebd",
+            "~i": "\u0129",
+            "~n": "\u00f1",
+            "~o": "\u00f5",
+            "~u": "\u0169",
+            "~v": "\u1e7d",
+            "~y": "\u1ef9",
+            "\u00a8 ": "\u00a8",
+            "\u00a8A": "\u00c4",
+            "\u00a8E": "\u00cb",
+            "\u00a8H": "\u1e26",
+            "\u00a8I": "\u00cf",
+            "\u00a8O": "\u00d6",
+            "\u00a8U": "\u00dc",
+            "\u00a8W": "\u1e84",
+            "\u00a8X": "\u1e8c",
+            "\u00a8Y": "\u0178",
+            "\u00a8a": "\u00e4",
+            "\u00a8e": "\u00eb",
+            "\u00a8h": "\u1e27",
+            "\u00a8i": "\u00ef",
+            "\u00a8o": "\u00f6",
+            "\u00a8t": "\u1e97",
+            "\u00a8u": "\u00fc",
+            "\u00a8w": "\u1e85",
+            "\u00a8x": "\u1e8d",
+            "\u00a8y": "\u00ff",
+            "^ ": "^",
+            "^A": "\u00c2",
+            "^C": "\u0108",
+            "^E": "\u00ca",
+            "^G": "\u011c",
+            "^H": "\u0124",
+            "^I": "\u00ce",
+            "^J": "\u0134",
+            "^O": "\u00d4",
+            "^S": "\u015c",
+            "^U": "\u00db",
+            "^W": "\u0174",
+            "^Y": "\u0176",
+            "^Z": "\u1e90",
+            "^a": "\u00e2",
+            "^c": "\u0109",
+            "^e": "\u00ea",
+            "^g": "\u011d",
+            "^h": "\u0125",
+            "^i": "\u00ee",
+            "^j": "\u0135",
+            "^o": "\u00f4",
+            "^s": "\u015d",
+            "^u": "\u00fb",
+            "^w": "\u0175",
+            "^y": "\u0177",
+            "^z": "\u1e91",
+            "\u00b4 ": "\u00b4",
+            "\u00b4A": "\u00c1",
+            "\u00b4C": "\u0106",
+            "\u00b4E": "\u00c9",
+            "\u00b4G": "\u01f4",
+            "\u00b4I": "\u00cd",
+            "\u00b4K": "\u1e30",
+            "\u00b4L": "\u0139",
+            "\u00b4M": "\u1e3e",
+            "\u00b4N": "\u0143",
+            "\u00b4O": "\u00d3",
+            "\u00b4P": "\u1e54",
+            "\u00b4R": "\u0154",
+            "\u00b4S": "\u015a",
+            "\u00b4U": "\u00da",
+            "\u00b4W": "\u1e82",
+            "\u00b4Y": "\u00dd",
+            "\u00b4Z": "\u0179",
+            "\u00b4a": "\u00e1",
+            "\u00b4c": "\u0107",
+            "\u00b4e": "\u00e9",
+            "\u00b4g": "\u01f5",
+            "\u00b4i": "\u00ed",
+            "\u00b4k": "\u1e31",
+            "\u00b4l": "\u013a",
+            "\u00b4m": "\u1e3f",
+            "\u00b4n": "\u0144",
+            "\u00b4o": "\u00f3",
+            "\u00b4p": "\u1e55",
+            "\u00b4r": "\u0155",
+            "\u00b4s": "\u015b",
+            "\u00b4u": "\u00fa",
+            "\u00b4w": "\u1e83",
+            "\u00b4y": "\u00fd",
+            "\u00b4z": "\u017a",
+            "\u00b4\u00c5": "\u01fa",
+            "\u00b4\u00c6": "\u01fc",
+            "\u00b4\u00d8": "\u01fe",
+            "\u00b4\u00e5": "\u01fb",
+            "\u00b4\u00e6": "\u01fd",
+            "\u00b4\u00f8": "\u01ff",
+            "` ": "`",
+            "`A": "\u00c0",
+            "`E": "\u00c8",
+            "`I": "\u00cc",
+            "`N": "\u01f8",
+            "`O": "\u00d2",
+            "`U": "\u00d9",
+            "`W": "\u1e80",
+            "`Y": "\u1ef2",
+            "`a": "\u00e0",
+            "`e": "\u00e8",
+            "`i": "\u00ec",
+            "`n": "\u01f9",
+            "`o": "\u00f2",
+            "`u": "\u00f9",
+            "`w": "\u1e81",
+            "`y": "\u1ef3"
+        }
+    };
+    Keyboard.layouts['ukr']={//乌克兰语      uk_101
+        'name' : 'Ukrainian',
+        'lang' : 'ukr',
+        "lay":{
+            'normal' : [
+                "' 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+                "\u0439 \u0446 \u0443 \u043a \u0435 \u043d \u0433 \u0448 \u0449 \u0437 \u0445 \u0457",
+                "{change} \u0444 \u0456 \u0432 \u0430 \u043f \u0440 \u043e \u043b \u0434 \u0436 \u0454 \u0491",
+                "{shift} \u044f \u0447 \u0441 \u043c \u0438 \u0442 \u044c \u0431 \u044e . {shift}",
+                "{alt} &nbsp {alt}"
+            ],
+            'shift' : [
+                '\u20b4 ! " \u2116 ; % : ? * ( ) _ + {bksp}',
+                "\u0419 \u0426 \u0423 \u041a \u0415 \u041d \u0413 \u0428 \u0429 \u0417 \u0425 \u0407",
+                "{change} \u0424 \u0406 \u0412 \u0410 \u041f \u0420 \u041e \u041b \u0414 \u0416 \u0404 \u0490",
+                "{shift} \u042f \u0427 \u0421 \u041c \u0418 \u0422 \u042c \u0411 \u042e , {shift}",
+                "{alt} &nbsp {alt}"
+            ],
+            'alt' : [
+                "{} {} {} {} {} {} {} {} {} {} {} {} {} {bksp}",
+                "{} {} {} {} {} {} \\ {} {} {} {} {}",
+                "{change} {} {} {} {} {} {} {} {} {} {} {} {}",
+                "{shift} {} {} {} {} {} {} {} {} {} {} {shift}",
+                "{alt} &nbsp {alt}"
+            ],
+            'alt-shift' : [
+                "{} {} {} {} {} {} {} {} {} {} {} {} {} {bksp}",
+                "{} {} {} {} {} {} / {} {} {} {} {}",
+                "{change} {} {} {} {} {} {} {} {} {} {} {} {}",
+                "{shift} {} {} {} {} {} {} {} {} {} {} {shift}",
+                "{alt} &nbsp {alt}"
+            ]
         }
     };
     win.Keyboard=Keyboard;
