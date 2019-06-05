438cd6d359dd1e59a4203f39bf4f3bf163a123be
xuexiang.yang <xuexiang.yang@iservernetwork.com>
2019-06-05 18:09:42 +0800

    提交

---
+++ vue/demo2/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>{{hello}}</div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            hello:"vue"
+        }
+    }
+}
+</script>
+<style scoped>
+div{
+    color: aquamarine;
+    font-size: 20px;
+}
+</style>
+
+
